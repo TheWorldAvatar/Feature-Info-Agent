9e9bf475a72b4385c0d0cb992cce3e61d6f3e356
Oisín <opeppard@cmcl.io>
2025-02-14 16:12:51 +0000

    Update-workflows-for-CICD: tidy maven credential script

--- .github/scripts/set-maven-credentials.sh
+++ .github/scripts/set-maven-credentials.sh
@@ -22,7 +22,6 @@ if [[ -z "${MAVEN_USERNAME}" ]]; then
   echo "ERROR: The environment variable 'MAVEN_USERNAME' has not been set."
   exit 1
 else
-  MAVEN_USERNAME="${MAVEN_USERNAME}"
   echo "Retrieved the 'MAVEN_USERNAME' environment variable."
 fi
 
@@ -31,17 +30,15 @@ if [[ -z "${MAVEN_PASSWORD}" ]]; then
   echo "ERROR: The environment variable 'MAVEN_PASSWORD' has not been set."
   exit 1
 else
-  MAVEN_PASSWORD="${MAVEN_PASSWORD}"
   echo "Retrieved the 'MAVEN_PASSWORD' environment variable."
 fi
 
-
 # Copy the template settings file to output location
-cp ./template-maven-settings.xml $TARGET_DIR/settings.xml
+cp ./template-maven-settings.xml "$TARGET_DIR"/settings.xml
 
 # Inject the maven credentials into new settings file
-sed -i "s/REPO_USERNAME/$MAVEN_USERNAME/" $TARGET_DIR/settings.xml
-sed -i "s/REPO_PASSWORD/$MAVEN_PASSWORD/" $TARGET_DIR/settings.xml
+sed -i "s/REPO_USERNAME/$MAVEN_USERNAME/" "$TARGET_DIR"/settings.xml
+sed -i "s/REPO_PASSWORD/$MAVEN_PASSWORD/" "$TARGET_DIR"/settings.xml
 
 # Finish
 echo "Script finished successfully, 'settings.xml' file can now be used."
